51fe78d12f2ac22e66c576bf1b40a0f02c205dee
thebluedragon7 <71325159+Thebluedragon7@users.noreply.github.com>
2024-02-26 21:54:21 +0545

    UPDATED: Dependencies in pubspec.yaml; ADDED: Forgot Password Pages (unstyled) with required asset

--- lib/pages/forgot_password/forgot_password_page.dart
+++ lib/pages/forgot_password/forgot_password_page.dart
@@ -1,3 +1,5 @@
+import 'package:ampushare/pages/login/login_page.dart';
+import 'package:ampushare/widgets/layouts/auth_layout/auth_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -6,6 +8,50 @@ class ForgotPasswordPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          const AuthLayout(),
+          SingleChildScrollView(
+            child: SizedBox(
+              height: screenHeight,
+              width: screenWidth,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Card(
+                    child: SizedBox(
+                      width: screenWidth * 0.85,
+                      height: screenHeight * 0.45,
+                      child: Column(
+                        children: [
+                          Text("Send OTP to your Email"),
+                          TextField(),
+                          Text("*check your inbox or your spam folder"),
+                          OutlinedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginPage(),
+                                  ),
+                                );
+                              },
+                              child: const Text("Back to Login"))
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
